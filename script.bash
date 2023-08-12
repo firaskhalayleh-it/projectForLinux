6f35b1504a0a095dfa13cc0ab916afbc753be48d
Feras Khalil Musallam Alkhalailah <91515883+firaskhalayleh-it@users.noreply.github.com>
2023-08-12 19:14:46 +0300

    editing the code

--- script.bash
+++ script.bash
@@ -4,19 +4,12 @@ logging="$HOME/logging"
 
 mkdir -p "$logging"
 
-CombinedLog="$logging/cumulative-kernel-logs.txt"
-CompressedCombinedLog="$logging/cumulative-kernel-logs.tar.bz2"
+CurrentDate=$(date +"%Y-%m-%dT%H-%M")
 
+LogFilename="$logging/kernel-logs_${CurrentDate}.txt"
+CompressedFilename="$logging/kernel-logs_${CurrentDate}.tar.bz2"
 
-    CurrentDate=$(date +"%Y-%m-%dT%H-%M")
-
-    LogFilename="$logging/kernel-logs_${CurrentDate}.txt"
-    CompressedFilename="$logging/kernel-logs_${CurrentDate}.tar.bz2"
-
-    journalctl --system --since "1 hour ago" > "$LogFilename"
-    cat "$LogFilename" >> "$CombinedLog"
-    tar -cvjf "$CompressedFilename" "$LogFilename"
-    tar --append -f "$CompressedCombinedLog" "$LogFilename"
-    dbxcli put "$CompressedCombinedLog" "/Dropbox/logs/"
-    rm "$CompressedFilename" "$LogFilename"
+journalctl --since "1 hour ago" -k > "$LogFilename"
+tar -cvjf "$CompressedFilename" "$LogFilename"
+dbxcli put "$CompressedFilename" "/Dropbox/logs/"
 
