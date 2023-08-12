e01b50ca5afbaff9633f51e297dc36f68809b190
Feras Khalil Musallam Alkhalailah <91515883+firaskhalayleh-it@users.noreply.github.com>
2023-08-12 06:53:31 +0300

    creating script.bash and adding the shell script

---
+++ script.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+logging="$HOME/logging"
+
+mkdir -p "$logging"
+
+CombinedLog="$logging/cumulative-kernel-logs.txt"
+CompressedCombinedLog="$logging/cumulative-kernel-logs.tar.bz2"
+
+while true; do
+    CurrentDate=$(date +"%Y-%m-%dT%H-%M")
+
+    LogFilename="$logging/kernel-logs_${CurrentDate}.txt"
+    CompressedFilename="$logging/kernel-logs_${CurrentDate}.tar.bz2"
+
+    journalctl --system --since "1 hour ago" > "$LogFilename"
+    cat "$LogFilename" >> "$CombinedLog"
+    tar -cvjf "$CompressedFilename" "$LogFilename"
+    tar --append -f "$CompressedCombinedLog" "$LogFilename"
+    dbxcli put "$CompressedCombinedLog" "/Dropbox/logs/"
+    rm "$CompressedFilename" "$LogFilename"
+done
